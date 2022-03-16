5226f48ee8c56b01cd933108dd3daf02732669d5
Loly Maluf <lolymaluf@gmail.com>
2022-03-15 23:15:04 -0300

    adiciona questoes 08 e 09

--- M1-FUNDAMENTOS/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/exercicios.js
+++ M1-FUNDAMENTOS/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/exercicios.js
@@ -4,60 +4,53 @@ novoH1.innerText = 'Exercício 5.2 - JavaScript DOM';
 document.body.appendChild(novoH1);
 
 /* exercicio 02 */
-let main = document.createElement('main');
-main.className = 'main-content';
-document.body.appendChild(main);
+let main1 = document.createElement('main');
+main1.className = 'main-content';
+document.body.appendChild(main1);
 
 /* Exercicio 03 */
-let section = document.createElement('section');
-section.className = 'center-content';
-main.appendChild(section);
+let section1 = document.createElement('section');
+section1.className = 'center-content';
+main1.appendChild(section1);
 
 /* Exercicio 04 */
 let p1 = document.createElement('p');
 p1.innerText = 'Paragrafo escrito alguma coisa';
-section.appendChild(p1);
+section1.appendChild(p1);
 
 /* Exercicio 05 */
 let sectionL = document.createElement('section');
-section.className = 'left-content';
-main.appendChild(sectionL);
+sectionL.className = 'left-content';
+main1.appendChild(sectionL);
 
 /* Exercicio 06 */
 let sectionR = document.createElement('section');
-section.className = 'right-content';
-main.appendChild(sectionR);
+sectionR.className = 'right-content';
+main1.appendChild(sectionR);
 
 /* Exercicio 07 */
 let image = document.createElement('img');
-section.className = 'small-image';
+image.className = 'small-image';
 image.src = 'https://picsum.photos/200'
 sectionL.appendChild(image);
 
 /* Exercicio 08 */
 let lista = document.createElement('ul');
+let numeros = ['um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez'];
+
+for(let index in numeros){
+    let itensLista = document.createElement('li')
+    itensLista.innerText = numeros[index]
+    lista.appendChild(itensLista)}
+    
 sectionR.appendChild(lista)
-let itensLista = document.createElement('li')
-itensLista.innerText ['um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez'] ;
-lista.appendChild(itensLista)
 
 /* Exercicio 09 */
 function createElement(titulos) {
     let tituloh3 = document.createElement('h3')
-    tituloh3.classList = titulos
+    tituloh3.classList = 'titulos'
+    return tituloh3
 }
-main.appendChild(createElement('h3-1'))
-main.appendChild(createElement('h3-2'))
-main.appendChild(createElement('h3-3'))
-/* 
-let h31 = document.createElement('h3');
-h31.innerText = 'h3 - 1';
-main.appendChild(h31);
-
-let h32 = document.createElement('h3');
-h32.innerText = 'h3 - 2';
-main.appendChild(h32);
-
-let h33 = document.createElement('h3');
-h33.innerText = 'h3 - 3';
-main.appendChild(h33); */
+main1.appendChild(createElement('h3-1'))
+main1.appendChild(createElement('h3-2'))
+main1.appendChild(createElement('h3-3'))
