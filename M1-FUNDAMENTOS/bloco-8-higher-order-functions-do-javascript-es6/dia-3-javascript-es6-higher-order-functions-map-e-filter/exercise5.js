f785ca94e828c8905f9161baf401b2a6bfad4646
Loly Maluf <lolymaluf@gmail.com>
2022-04-11 22:32:06 -0300

    Adiciona Exercicio 05

--- M1-FUNDAMENTOS/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercise5.js
+++ M1-FUNDAMENTOS/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercise5.js
@@ -1,75 +1,93 @@
 const books = [
-    {
-      id: 1,
-      name: 'As Crônicas de Gelo e Fogo',
-      genre: 'Fantasia',
-      author: {
-        name: 'George R. R. Martin',
-        birthYear: 1948,
-      },
-      releaseYear: 1991,
+  {
+    id: 1,
+    name: "As Crônicas de Gelo e Fogo",
+    genre: "Fantasia",
+    author: {
+      name: "George R. R. Martin",
+      birthYear: 1948,
     },
-    {
-      id: 2,
-      name: 'O Senhor dos Anéis',
-      genre: 'Fantasia',
-      author: {
-        name: 'J. R. R. Tolkien',
-        birthYear: 1892,
-      },
-      releaseYear: 1954,
+    releaseYear: 1991,
+  },
+  {
+    id: 2,
+    name: "O Senhor dos Anéis",
+    genre: "Fantasia",
+    author: {
+      name: "J. R. R. Tolkien",
+      birthYear: 1892,
     },
-    {
-      id: 3,
-      name: 'Fundação',
-      genre: 'Ficção Científica',
-      author: {
-        name: 'Isaac Asimov',
-        birthYear: 1920,
-      },
-      releaseYear: 1951,
+    releaseYear: 1954,
+  },
+  {
+    id: 3,
+    name: "Fundação",
+    genre: "Ficção Científica",
+    author: {
+      name: "Isaac Asimov",
+      birthYear: 1920,
     },
-    {
-      id: 4,
-      name: 'Duna',
-      genre: 'Ficção Científica',
-      author: {
-        name: 'Frank Herbert',
-        birthYear: 1920,
-      },
-      releaseYear: 1965,
+    releaseYear: 1951,
+  },
+  {
+    id: 4,
+    name: "Duna",
+    genre: "Ficção Científica",
+    author: {
+      name: "Frank Herbert",
+      birthYear: 1920,
     },
-    {
-      id: 5,
-      name: 'A Coisa',
-      genre: 'Terror',
-      author: {
-        name: 'Stephen King',
-        birthYear: 1947,
-      },
-      releaseYear: 1986,
+    releaseYear: 1965,
+  },
+  {
+    id: 5,
+    name: "A Coisa",
+    genre: "Terror",
+    author: {
+      name: "Stephen King",
+      birthYear: 1947,
     },
-    {
-      id: 6,
-      name: 'O Chamado de Cthulhu',
-      genre: 'Terror',
-      author: {
-        name: 'H. P. Lovecraft',
-        birthYear: 1890,
-      },
-      releaseYear: 1928,
+    releaseYear: 1986,
+  },
+  {
+    id: 6,
+    name: "O Chamado de Cthulhu",
+    genre: "Terror",
+    author: {
+      name: "H. P. Lovecraft",
+      birthYear: 1890,
     },
-  ];
-  
-  const expectedResult = [
-    'Frank Herbert',
-    'George R. R. Martin',
-    'Isaac Asimov',
-    'J. R. R. Tolkien',
-  ];
+    releaseYear: 1928,
+  },
+];
 
-  /* 5 - Crie um array em ordem alfabética apenas com os nomes de todas as pessoas autoras de ficção científica ou fantasia. */
-  
-  function fantasyOrScienceFictionAuthors() {
-    // escreva seu código aqui
+const expectedResult = [
+  "Frank Herbert",
+  "George R. R. Martin",
+  "Isaac Asimov",
+  "J. R. R. Tolkien",
+];
+
+/* 5 - Crie um array em ordem alfabética apenas com os nomes de todas as pessoas autoras de ficção científica ou fantasia. */
+
+function fantasyOrScienceFictionAuthors() {
+  const generoFF = ["Ficção Científica", "Fantasia"];
+  const imprimirFF = books
+    .filter((livrosFF) => generoFF.includes(livrosFF.genre))
+    .map((livrosFF) => livrosFF.author.name)
+    .sort();
+  console.log(imprimirFF);
+}
+fantasyOrScienceFictionAuthors();
+
+/* FUNÇAO ANTERIOR UTILIZANDO FILTER */
+/*   function fantasyOrScienceFictionAuthors() {
+    let generoFiccao = books.filter ((ficcao) => ficcao.genre === 'Ficção Científica')
+    autorFiccao = generoFiccao.author.name;
+    let generoFantasia = books.filter ((fantasia) => fantasia.genre === 'Fantasia')
+    autorFantasia = generoFantasia.author.name;
+    ficcaoFantasia = autorFiccao + autorFantasia
+    .sort((a, b) => (a.ficcaoFantasia) - (b.ficcaoFantasia));
+    console.log(autorFF);
   }
+  fantasyOrScienceFictionAuthors(); */
